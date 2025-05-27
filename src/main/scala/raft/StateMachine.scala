5cdc2afe102f238b5b8dbc43f9f8c5db0962a648
Nick <nomail>
2025-05-26 20:25:42 -0400

    decrement state machine

--- src/main/scala/raft/StateMachine.scala
+++ src/main/scala/raft/StateMachine.scala
@@ -18,3 +18,11 @@ final class LastValue[T <: Serializable] extends StateMachine[T, T] {
     copy
   }
 }
+
+final class DecrementCounter[T <: Serializable](init: Int = 0) extends StateMachine[T, Int] {
+  private var value: Int = init
+
+  override def apply(value: T): Unit = this.value -= 1
+  override def state(): Int = this.value
+  override def copy(): StateMachine[T, Int] = new DecrementCounter(this.value)
+}
